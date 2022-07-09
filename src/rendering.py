22409f0ce97b20daeed5f4ccad13e0d78ffa3a3b
davidpendergast <dlp75@case.edu>
2022-07-08 23:30:58 -0400

    improved render ordering when things overlap walls

--- src/rendering.py
+++ src/rendering.py
@@ -33,6 +33,12 @@ class LevelRenderer:
     def update(self):
         pass
 
+    def all_sorted_entities_to_render(self):
+        if self.cur_state is not None:
+            for xy in self.cur_state.level:
+                for ent in self.cur_state.level[xy]:
+                    yield ent, xy
+
     def draw_entity_at(self, ent, surf, xy):
         ent_sprite = ent.get_sprite(self.cell_size)
         ent_xy = (round(self.xy_offset[0] + self.cell_size * xy[0]),
@@ -40,10 +46,8 @@ class LevelRenderer:
         surf.blit(ent_sprite, ent_xy)
 
     def draw(self, surf):
-        if self.cur_state is not None:
-            for xy in self.cur_state.level:
-                for ent in self.cur_state.level[xy]:
-                    self.draw_entity_at(ent, surf, xy)
+        for ent, xy in self.all_sorted_entities_to_render():
+            self.draw_entity_at(ent, surf, xy)
 
 
 _STATIONARY = 0
@@ -58,59 +62,58 @@ class AnimatedLevelRenderer(LevelRenderer):
         super().__init__(state, cell_size=cell_size)
         self.trans_time = trans_time  # seconds
 
-        self.fancy_overlaps = True
         self.spin_hz = 1
         self.fancy_radius = 0.25
 
-    def draw(self, surf):
+    def all_sorted_entities_to_render(self):
+        if self.cur_state is None:
+            return ()
+
+        walls = []
+        nonwalls = []
+
         cur_time = inputs.get_time()
         if cur_time > self.prev_state_time + self.trans_time or self.prev_state is None:
-            if not self.fancy_overlaps:
-                super().draw(surf)
-            else:
-                if self.cur_state is not None:
-                    for xy in self.cur_state.level:
-                        nonwalls = []
-                        for ent in self.cur_state.level[xy]:
-                            if ent.is_wall():
-                                self.draw_entity_at(ent, surf, xy)
-                            else:
-                                nonwalls.append(ent)
+            # we're not mid-update
+            for xy in self.cur_state.level:
+                temp_nonwalls = []
+                for ent in self.cur_state.level[xy]:
+                    if ent.is_wall():
+                        walls.append((ent, xy))
+                    else:
+                        temp_nonwalls.append((ent, xy))
 
-                        if len(nonwalls) <= 1:
-                            for ent in nonwalls:
-                                self.draw_entity_at(ent, surf, xy)
-                        else:
-                            to_draw = []
-                            for i, ent in enumerate(sorted(nonwalls, key=lambda _ent: _ent.uid)):
-                                t = 2 * math.pi * (cur_time * self.spin_hz + i / len(nonwalls))
-                                fancy_x = xy[0] + self.fancy_radius * math.cos(t)
-                                fancy_y = xy[1] + self.fancy_radius * math.sin(t) / 2
-                                to_draw.append((fancy_y, fancy_x, ent))
-
-                            to_draw.sort()
-                            for (y, x, ent) in to_draw:
-                                self.draw_entity_at(ent, surf, (x, y))
+                if len(temp_nonwalls) <= 1:
+                    nonwalls.extend(temp_nonwalls)
+                else:
+                    for i, (ent, _) in enumerate(sorted(temp_nonwalls, key=lambda e_xy: e_xy[0].uid)):
+                        t = 2 * math.pi * (cur_time * self.spin_hz + i / len(temp_nonwalls))
+                        fancy_x = xy[0] + self.fancy_radius * math.cos(t)
+                        fancy_y = xy[1] + self.fancy_radius * math.sin(t) / 2
+                        nonwalls.append((ent, (fancy_x, fancy_y)))
         else:
+            # we're interpolating
             cur_ents = {e_xy[0]: e_xy[1] for e_xy in self.cur_state.all_entity_positions()}
             old_ents = {e_xy[0]: e_xy[1] for e_xy in self.prev_state.all_entity_positions()}
             interp = (cur_time - self.prev_state_time) / self.trans_time
 
-            to_render = []
-
-            for e in old_ents:
-                if e not in cur_ents:
-                    to_render.append((old_ents[e], e, _DEAD))
-                elif old_ents[e] != cur_ents[e]:
-                    xy_interp = utils.interpolate(old_ents[e], cur_ents[e], interp, rounded=False)
-                    to_render.append((xy_interp, e, _MOVING))
-                else:
-                    to_render.append((cur_ents[e], e, _STATIONARY))
             for e in cur_ents:
                 if e not in old_ents:
-                    to_render.append((cur_ents[e], e, _NEW))
+                    nonwalls.append((e, cur_ents[e]))  # newly spawned?
+                elif cur_ents[e] != old_ents[e]:
+                    xy_interp = utils.interpolate(old_ents[e], cur_ents[e], interp, rounded=False)
+                    nonwalls.append((e, xy_interp))  # it moved
+                else:
+                    nonwalls.append((e, cur_ents[e]))  # it didn't move
+            for e in old_ents:
+                if e not in cur_ents:
+                    nonwalls.append((e, old_ents[e]))  # it died
 
-            to_render.sort(key=lambda e_tup: (e_tup[0][1], e_tup[2], random.random()))
+        walls.sort(key=lambda w_xy: (w_xy[1][1], w_xy[1][0], w_xy[0]))
+        for w_xy in walls:
+            yield w_xy
 
-            for xy, ent, ent_state in to_render:
-                self.draw_entity_at(ent, surf, xy)
+        nonwalls.sort(key=lambda e_xy: (e_xy[1][1], e_xy[1][0], e_xy[0]))
+        for e_xy in nonwalls:
+            yield e_xy
+
