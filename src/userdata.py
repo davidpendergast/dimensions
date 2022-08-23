047a46e473dbc1880ddeb84f4d389863d799efb9
davidpendergast <dlp75@case.edu>
2022-08-23 03:41:45 -0400

    rearranged userdata.py to make it more readible

--- src/userdata.py
+++ src/userdata.py
@@ -10,25 +10,24 @@ import json
 _ACTUALLY_RUNNING_IN_WEB_MODE = False
 _HAS_APPDIRS = False
 if platform.system().lower() == "emscripten":
-    # note that this stuff only resolves when running in a web context
+    # Note that this stuff only resolves when running in a web context
     if __WASM__ and __EMSCRIPTEN__ and __EMSCRIPTEN__.is_browser:
         from __EMSCRIPTEN__ import window, document
     _ACTUALLY_RUNNING_IN_WEB_MODE = True
 else:
     try:
-        import appdirs  # doesn't seem to resolve in web-mode
+        import appdirs  # 3rd party module: https://pypi.org/project/appdirs/
         _HAS_APPDIRS = True
     except ImportError:
         print("WARN: appdirs module couldn't be imported. userdata.USER_DATA_DIR mode will not be available.")
 
 
 # Storage Modes
-USER_DATA_DIR = "userdata_directory"
-CURRENT_WORKING_DIR = "working_directory"
-LOCAL_WEB_STORAGE = "browser_storage"
-SAVE_AND_LOAD_DISABLED = "no_save_and_load"
-
-BEST = "best"
+USER_DATA_DIR = "USER_DATA_DIR"
+CURRENT_WORKING_DIR = "CURRENT_WORKING_DIR"
+LOCAL_WEB_STORAGE = "LOCAL_WEB_STORAGE"
+SAVE_AND_LOAD_DISABLED = "SAVE_AND_LOAD_DISABLED"
+BEST = "BEST"
 
 
 # Metadata
@@ -72,62 +71,6 @@ def initialize(keyname: str, mode: str = BEST, appname=None, appauthor=None, ver
     _APPNAME = appname
     _APPAUTHOR = appauthor
     _VERSION = version
-
-
-def _set_mode(mode):
-    if mode == BEST:
-        if _ACTUALLY_RUNNING_IN_WEB_MODE:
-            mode = LOCAL_WEB_STORAGE
-        else:
-            mode = USER_DATA_DIR if _HAS_APPDIRS else CURRENT_WORKING_DIR
-
-    if mode == LOCAL_WEB_STORAGE and not _ACTUALLY_RUNNING_IN_WEB_MODE:
-        raise ValueError(f"Cannot use saving mode \"{mode}\" if not running in web mode.")
-    elif mode in (LOCAL_WEB_STORAGE, CURRENT_WORKING_DIR, USER_DATA_DIR, SAVE_AND_LOAD_DISABLED):
-        global _MODE
-        _MODE = mode
-    else:
-        raise ValueError(f"Unrecognized saving mode: \"{mode}\"")
-
-
-def _check_initialized():
-    if _KEY is None:
-        raise ValueError("module has not been initialized.")
-
-
-_VALID_CHARS_FOR_FP = set(s for s in string.ascii_letters + "0123456789_()")
-
-
-def _clean_for_fp(text, replacewith=""):
-    if text is None:
-        return None
-    else:
-        return "".join((t if t in _VALID_CHARS_FOR_FP else replacewith) for t in text)
-
-
-def _copy_via_json_serialization(val):
-    try:
-        val_as_json_str = json.dumps(val)
-        return json.loads(val_as_json_str)
-    except Exception as e:
-        print(f"ERROR: val isn't json-compatible: {val} (type is {type(val).__name__})")
-        raise e
-
-
-def _get_local_filepath(mkdirs_if_necessary=True) -> str:
-    filename = f"{_KEY}.json"
-    if _MODE == USER_DATA_DIR:
-        directory = appdirs.user_data_dir(
-            appname=_clean_for_fp(_APPNAME),
-            appauthor=_clean_for_fp(_APPAUTHOR))
-
-        if mkdirs_if_necessary and not os.path.exists(directory):
-            print(f"INFO: creating {directory}")
-            os.makedirs(directory, exist_ok=True)
-
-        return os.path.join(directory, filename)
-    else:
-        return filename
 
 
 def load_data_from_disk() -> bool:
@@ -201,6 +144,55 @@ def save_data_to_disk(force=False) -> bool:
     return True
 
 
+def set_data(key: str, val, and_save_to_disk=True):
+    """Creates or updates a key-value pair of data in program memory.
+
+        Args:
+            key (str): the key for the data.
+            val: the data to store. Must be json-compatible.
+            and_save_to_disk (bool): Whether to save this change to disk immediately.
+    """
+    _check_initialized()
+
+    global _IN_MEMORY, _DIRTY
+    val = _copy_via_json_serialization(val)
+    if key not in _IN_MEMORY or _IN_MEMORY[key] != val:
+        _DIRTY = True
+    _IN_MEMORY[key] = val
+
+    if and_save_to_disk:
+        save_data_to_disk()
+
+
+def get_data(key: str, coercer=lambda x: x, or_else=None):
+    """Fetches a piece of data from program memory using its key.
+
+        Args:
+            key (str): the key for the data.
+            coercer: an optional function that converts or cleans the data before it's returned. For example,
+                if you're expecting the data to be an integer, you may want to pass `coercer=int` to guarantee the
+                return value is an int. This way callers can prevent themselves from receiving corrupted data to
+                some degree.
+            or_else: a value to return if the data isn't present or caused `coercer` to throw an error.
+    """
+    _check_initialized()
+
+    if key in _IN_MEMORY:
+        val = _IN_MEMORY[key]
+        val = _copy_via_json_serialization(val)
+        if coercer is not None:
+            try:
+                return coercer(val)
+            except Exception:
+                print(f"ERROR: failed to coerce user data: {key}={val} (using {or_else} instead)")
+                traceback.print_exc()
+                return or_else
+        else:
+            return val
+
+    return or_else
+
+
 def reset_data(hard=False):
     """Erases the user's data in program memory.
 
@@ -241,50 +233,59 @@ def reset_data(hard=False):
     return True
 
 
-def get_data(key: str, coercer=lambda x: x, or_else=None):
-    """Fetches a piece of data from program memory using its key.
-
-        Args:
-            key (str): the key for the data.
-            coercer: an optional function that converts or cleans the data before it's returned. For example,
-                if you're expecting the data to be an integer, you may want to pass `coercer=int` to guarantee the
-                return value is an int. This way callers can prevent themselves from receiving corrupted data to
-                some degree.
-            or_else: a value to return if the data isn't present or caused `coercer` to throw an error.
-    """
-    _check_initialized()
-
-    if key in _IN_MEMORY:
-        val = _IN_MEMORY[key]
-        val = _copy_via_json_serialization(val)
-        if coercer is not None:
-            try:
-                return coercer(val)
-            except Exception:
-                print(f"ERROR: failed to coerce user data: {key}={val} (using {or_else} instead)")
-                traceback.print_exc()
-                return or_else
+def _set_mode(mode):
+    if mode == BEST:
+        if _ACTUALLY_RUNNING_IN_WEB_MODE:
+            mode = LOCAL_WEB_STORAGE
         else:
-            return val
+            mode = USER_DATA_DIR if _HAS_APPDIRS else CURRENT_WORKING_DIR
 
-    return or_else
+    if mode == LOCAL_WEB_STORAGE and not _ACTUALLY_RUNNING_IN_WEB_MODE:
+        raise ValueError(f"Cannot use saving mode \"{mode}\" if not running in web mode.")
+    elif mode == USER_DATA_DIR and not _HAS_APPDIRS:
+        raise ValueError(f"Cannot use saving mode \"{mode}\" without appdirs module.")
+    elif mode in (LOCAL_WEB_STORAGE, CURRENT_WORKING_DIR, USER_DATA_DIR, SAVE_AND_LOAD_DISABLED):
+        global _MODE
+        _MODE = mode
+    else:
+        raise ValueError(f"Unrecognized saving mode: \"{mode}\"")
 
 
-def set_data(key: str, val, and_save_to_disk=True):
-    """Creates or updates a key-value pair of data in program memory.
+def _check_initialized():
+    if _KEY is None:
+        raise ValueError("module has not been initialized.")
 
-        Args:
-            key (str): the key for the data.
-            val: the data to store. Must be json-compatible.
-            and_save_to_disk (bool): Whether to save this change to disk immediately.
-    """
-    _check_initialized()
 
-    global _IN_MEMORY, _DIRTY
-    val = _copy_via_json_serialization(val)
-    if key not in _IN_MEMORY or _IN_MEMORY[key] != val:
-        _DIRTY = True
-    _IN_MEMORY[key] = val
+_VALID_CHARS_FOR_FP = set(s for s in string.ascii_letters + "0123456789_()")
 
-    if and_save_to_disk:
-        save_data_to_disk()
+
+def _clean_for_fp(text, replacewith=""):
+    if text is None:
+        return None
+    else:
+        return "".join((t if t in _VALID_CHARS_FOR_FP else replacewith) for t in text)
+
+
+def _copy_via_json_serialization(val):
+    try:
+        val_as_json_str = json.dumps(val)
+        return json.loads(val_as_json_str)
+    except Exception as e:
+        print(f"ERROR: val isn't json-compatible: {val} (type is {type(val).__name__})")
+        raise e
+
+
+def _get_local_filepath(mkdirs_if_necessary=True) -> str:
+    filename = f"{_KEY}.json"
+    if _MODE == USER_DATA_DIR:
+        directory = appdirs.user_data_dir(
+            appname=_clean_for_fp(_APPNAME),
+            appauthor=_clean_for_fp(_APPAUTHOR))
+
+        if mkdirs_if_necessary and not os.path.exists(directory):
+            print(f"INFO: creating {directory}")
+            os.makedirs(directory, exist_ok=True)
+
+        return os.path.join(directory, filename)
+    else:
+        return filename
